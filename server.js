0443bf4973ccdda3e1e534a6ff057be443890610
glassysky <873661157@qq.com>
2016-04-19 23:31:28 +0800

    simple webpack template

---
+++ server.js
@@ -0,0 +1,58 @@
+/**
+ * Created by cuitianhao on 16/4/18.
+ */
+var express = require('express');
+var webpack = require('webpack');
+var http = require('http');
+var proxy = require('http-proxy-middleware');
+var config = require('./config');
+var webpackConfig = require('./webpack.config');
+var showFilesMiddleware = require('./middlewares/showFilesMiddleware');
+
+var port = process.env.PORT || 8000;
+
+var proxyTable = config.dev.proxyTable;
+
+var app = express();
+var compiler = webpack(webpackConfig);
+
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+        colors: true,
+        chunks: false
+    }
+});
+
+var hotMiddleware = require('webpack-hot-middleware')(compiler);
+
+
+
+
+// proxy api requests
+Object.keys(proxyTable).forEach(function (context) {
+    var options = proxyTable[context];
+    if (typeof options === 'string') {
+        options = { target: options };
+    }
+    app.use(proxyMiddleware(context, options));
+});
+
+// app.use(require('connect-history-api-fallback'));
+
+app.use(devMiddleware);
+
+app.use(hotMiddleware);
+
+//static sources
+app.use('/static', express.static('./static'));
+
+// app.get('/', showFilesMiddleware);
+
+module.exports = app.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Listening at http://localhost:' + port + '\n');
+});
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: ['./index.js', 'webpack-hot-middleware/client']
+    },
+    output: {
+        //real path
+        path: path.resolve(__dirname),
+        //in memory
+        publicPath: '/static/',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: [ 'react', 'es2015' ]
+                }
+            }
+        ]
+    },
+    plugins: [
+        // Webpack 1.0
+        new webpack.optimize.OccurenceOrderPlugin(),
+        // Webpack 2.0 fixed this mispelling
+        // new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        //react
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: false
+        })
+    ]
+};
